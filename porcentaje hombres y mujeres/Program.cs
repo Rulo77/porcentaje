ae7cf5ae41a7c1cabc9f5fc0d218e6586e0a1be3
Raul Gomez <ragomez1234567@gmail.com>
2021-09-30 18:12:42 -0600

    Add project files.

---
+++ porcentaje hombres y mujeres/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace porcentaje_hombres_y_mujeres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h, m, total, ph, pm;
+            string entrada1, entrada2;
+            Console.WriteLine("ingrese cantidad de hombres que hay en el grupo");
+            entrada1 = Console.ReadLine();
+
+            Console.WriteLine("ingrese cantidad de mujeres que hay en el grupo");
+            entrada2 = Console.ReadLine();
+
+            h = Convert.ToInt32(entrada1);
+            m = Convert.ToInt32(entrada2);
+
+            total = h + m;
+            ph = h * 100 / total;
+            pm = m * 100 / total;
+
+            
+            Console.WriteLine("el porcentaje de hombres es: {0} % y el de mujeres es: {1} %", ph,pm);
+            Console.Read();
+
+            
+        }
+    }
+}
